fbd3b9d692ab00629b668cb463dadbfa427a18da
juandahveed <jd@activeinternetmarketing.com>
2019-11-01 22:54:01 -0400

    okta authentication is working

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom';
 
+import { Security, ImplicitCallback } from '@okta/okta-react';
+
 import Home from '../pages/home';
 import About from '../pages/about';
 import Lessons from '../pages/lessons';
 import Schedule from '../pages/schedule';
 import Contact from '../pages/contact';
 import Notfound from '../pages/notfound';
+
+const config = {
+    issuer: 'https://dev-373610.okta.com/oauth2/default',
+    redirectUri: window.location.origin + '/implicit/callback',
+    clientId: '0oa1q3t4pyfd7fUHW357',
+    pkce: true
+}
 
 class Navigation extends React.Component {
     constructor() {
@@ -27,14 +36,17 @@ class Navigation extends React.Component {
                         </ul>
                     </nav>
                 </header>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/lessons" component={Lessons} />
-                    <Route exact path="/schedule" component={Schedule} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Route component={Notfound} />
-                </Switch>
+                <Security {...config}>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/lessons" component={Lessons} />
+                        <Route exact path="/schedule" component={Schedule} />
+                        <Route exact path="/contact" component={Contact} />
+                        <Route path='/implicit/callback' component={ImplicitCallback} />
+                        <Route component={Notfound} />
+                    </Switch>
+                </Security>
 
             </div>
         </Router>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import { withAuth } from '@okta/okta-react';
 
 class Home extends React.Component {
-    constructor() {
-        super();
-        // this.state = {color: 'red'};
+    constructor(props) {
+        super(props);
+        this.state = { authenticated: null };
+        this.checkAuthentication = this.checkAuthentication.bind(this);
+        this.checkAuthentication();
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
     }
+
+    async checkAuthentication() {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+            this.setState({ authenticated });
+        }
+    }
+
+    componentDidUpdate() {
+        this.checkAuthentication();
+    }
+
+    async login() {
+        // Redirect to '/' after login
+        this.props.auth.login('/about');
+    }
+
+    async logout() {
+        // Redirect to '/' after logout
+        this.props.auth.logout('/');
+    }
+
     render() {
-        return <div>
-            <h2>Please Login To Use The App</h2>
-            <form>
-                <input type="text" placeholder="username"></input>
-                <input type="password" placeholder="password"></input>
-            </form>
-        </div>
+        // return <div>
+        //     <h2>Please Login To Use The App</h2>
+        if (this.state.authenticated === null) return null;
+        return this.state.authenticated ?
+            <button onClick={this.logout}>Logout</button> :
+            <div><h2>Please Login To Use The App</h2>
+                <button onClick={this.login}>Login</button></div>;
+        // </div>
     }
 }
 
-export default Home;
+export default withAuth(Home);
